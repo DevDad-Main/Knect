4759ffc6763037d7fe54ae6e2e189c6f93775b6e
Oliver <softwaredevdad@gmail.com>
2025-08-24 12:04:23 +0200

    add all routes to our components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/pages/Login";
+import Feed from "./components/pages/Feed";
+import Messages from "./components/pages/Messages";
+import ChatBox from "./components/pages/ChatBox";
+import Connections from "./components/pages/Connections";
+import Discover from "./components/pages/Discover";
+import Profile from "./components/pages/Profile";
+import CreatePost from "./components/pages/CreatePost";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route index element={<Feed />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="messages:userId" element={<ChatBox />} />
+          <Route path="connections" element={<Connections />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/:profileId" element={<Profile />} />
+          <Route path="create-post" element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
