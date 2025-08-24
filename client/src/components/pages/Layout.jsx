f51d7187ff5e294843aa03d1b2ee443e4c4080a7
Oliver <softwaredevdad@gmail.com>
2025-08-24 14:35:13 +0200

    add main home page layout, checks to see if there is a user if not we display the loading component

--- client/src/components/pages/Layout.jsx
+++ client/src/components/pages/Layout.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import SideBar from "../SideBar";
+import { Outlet } from "react-router-dom";
+import { Menu, X } from "lucide-react";
+import { dummyUserData } from "../../assets/assets";
+import Loading from "../Loading";
 
 const Layout = () => {
-  return <div></div>;
+  const user = dummyUserData;
+  const [sideBarOpen, setSideBarOpen] = React.useState(false);
+
+  return user ? (
+    <div className="w-full flex h-screen">
+      <SideBar />
+      <div className="flex-1 bg-slate-50">
+        <Outlet />
+      </div>
+      {sideBarOpen ? (
+        <X
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSideBarOpen(false)}
+        />
+      ) : (
+        <Menu
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSideBarOpen(false)}
+        />
+      )}
+    </div>
+  ) : (
+    <Loading />
+  );
 };
 
 export default Layout;
