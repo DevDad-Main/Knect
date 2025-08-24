7b7c672de31ba608b346aa846bed94c2cc96867b
Oliver <softwaredevdad@gmail.com>
2025-08-24 18:22:10 +0200

    fix css where loading dosent load and not in correct position, add start component logic to feed, now to add css and map everything correctly from our dummy data

--- client/src/components/pages/Feed.jsx
+++ client/src/components/pages/Feed.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { dummyPostsData } from "../../assets/assets";
+import Loading from "../Loading";
 
 const Feed = () => {
-  return <div></div>;
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchFeeds = async () => {
+    setFeeds(dummyPostsData);
+  };
+
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+
+  return !loading ? (
+    <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
+      {/* Stories and List of Posts*/}
+      <div>
+        <h1>Stories </h1>
+        <div className="p-4 space-y-6">List of post here</div>
+      </div>
+
+      {/* Right sidebar*/}
+      <div></div>
+    </div>
+  ) : (
+    <Loading />
+  );
 };
 
 export default Feed;
