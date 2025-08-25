7364a721535e3f6337db0b286c4dc7b818a66580
Oliver <softwaredevdad@gmail.com>
2025-08-25 12:50:03 +0200

    finish messsages component till we hook up backend and add onclick event to users profile in the posts

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,6 +2,7 @@ import { BadgeCheck, Heart, MessageCircle, Share } from "lucide-react";
 import moment from "moment";
 import React, { useState, useEffect } from "react";
 import { dummyUserData } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const PostCard = ({ post }) => {
   const postWithHashtag = post.content.replace(
@@ -13,11 +14,15 @@ const PostCard = ({ post }) => {
   const currentUser = dummyUserData;
 
   const handleLike = async () => {};
+  const navigate = useNavigate();
 
   return (
     <div className="bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl">
       {/* User Info*/}
-      <div className="inline-flex items-center gap-3 cursor-pointer">
+      <div
+        onClick={() => navigate("/profile/" + post.user._id)}
+        className="inline-flex items-center gap-3 cursor-pointer"
+      >
         <img
           src={post.user?.profile_picture}
           className="w-10 h-10 rounded-full shadow"
