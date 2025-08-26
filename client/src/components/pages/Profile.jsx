9608cde1f522084d9bc1761cf7c53b72fda6bf02
Oliver <softwaredevdad@gmail.com>
2025-08-26 12:19:09 +0200

    complete discover page and usercard page until we can grab dynamic data from backend

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { dummyUserData, dummyPostsData } from "../assets/assets";
+import Loading from "../Loading";
 
 const Profile = () => {
-  return <div></div>;
+  const { profileId } = useParams();
+  const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [activeTab, setActiveTab] = useState("posts");
+  const [showEdit, setShowEdit] = useState(false);
+
+  const fetchUser = async () => {
+    setUser(dummyUserData);
+    setPosts(dummyPostsData);
+  };
+
+  // Whenever comp gets loaded we will fetch the users
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return user ? <div></div> : <Loading />;
 };
 
 export default Profile;
