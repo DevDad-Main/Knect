c1bcbd8889e1cb332957b815b164ec636ecffc7e
Oliver <softwaredevdad@gmail.com>
2025-08-29 18:09:21 +0200

    add recent messages, need to fix when added socket io

--- client/src/components/RecentMessages.jsx
+++ client/src/components/RecentMessages.jsx
@@ -2,17 +2,48 @@ import React, { useState, useEffect } from "react";
 import { dummyRecentMessagesData } from "../assets/assets";
 import { Link } from "react-router-dom";
 import moment from "moment";
+import { fetchData } from "./utils";
+import toast from "react-hot-toast";
 
 const RecentMessages = () => {
   const [messages, setMessages] = useState([]);
 
   const fetchRecentMessages = async () => {
-    setMessages(dummyRecentMessagesData);
+    try {
+      const data = await fetchData("api/v1/message/recent-messages");
+
+      if (data) {
+        console.log(data);
+        toast.success(data.message);
+        const groupedMessages = data.messages.reduce((acc, message) => {
+          const senderId = message.from_user_id._id;
+          if (
+            !acc[senderId] ||
+            new Date(message.createdAt) > new Date(acc[senderId].createdAt)
+          ) {
+            acc[senderId] = message;
+          }
+          return acc;
+        }, {});
+
+        // Sort messages by date
+        const sortedMessages = Object.values(groupedMessages).sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
+        );
+
+        setMessages(sortedMessages);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
-  useEffect(() => {
-    fetchRecentMessages();
-  }, []);
+  // useEffect(() => {
+  //   setInterval(fetchRecentMessages, 30000);
+  //   return () => {
+  //     clearInterval();
+  //   };
+  // }, []);
 
   return (
     <div className="bg-white max-w-xs mt-4 p-4 min-h-20 rounded-md shadow text-xs text-slate-800">
