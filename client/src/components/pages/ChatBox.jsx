0fa14f32bf7edee1e122699058fd0c80b64b43bd
Oliver <softwaredevdad@gmail.com>
2025-08-30 15:20:23 +0200

    test changing token sent to backend for socket io

--- client/src/components/pages/ChatBox.jsx
+++ client/src/components/pages/ChatBox.jsx
@@ -82,7 +82,7 @@ const ChatBox = () => {
     if (!token) return;
 
     socket.current = io(import.meta.env.VITE_BASEURL, {
-      query: { token },
+      query: { token: sessionStorage.getItem("token") },
       withCredentials: true,
     });
 
